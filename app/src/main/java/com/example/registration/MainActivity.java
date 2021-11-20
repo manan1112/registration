436acbc00311fe6495f6cd26847c9a66d4285bf6
manan1112 <mukhija.manan@gmail.com>
2021-11-20 15:17:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/registration/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.registration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+{
+    Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1=(Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view)
+            {
+                Toast.makeText(getApplicationContext(), " Registered Successfully ",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
