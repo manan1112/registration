7c1f18a44258af69b548371ed6cb8b52ccc5fef7
manan1112 <mukhija.manan@gmail.com>
2021-11-20 15:20:50 +0530

    first

--- app/src/main/java/com/example/registration/MainActivity.java
+++ app/src/main/java/com/example/registration/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity
 {
     Button b1;
     @Override
+    //hfvewkjfhklr
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
